7c4dea2fd192cbd0fdfbc01c1e9cd26573f2fe77
Isaac Underwood <undeis1@student.op.ac.nz>
2019-11-11 23:22:58 +1300

    update activity views

--- my-oranga/resources/views/activities/create.blade.php
+++ my-oranga/resources/views/activities/create.blade.php
@@ -11,31 +11,43 @@
         <form action="{{route('activities.store')}}" method="post" class="form">
             @csrf
             <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="start-date">Start Date</label>
-                    <input type="date" name="start_date" class="form-control" id="start-date" aria-describedby="dateHelp">
-                </div>
-                <div class="form-group col-md-6">
-                    <span>
-                        <label for="end-date" class="pr-4">End Date</label>
-                        <input type="checkbox" id="same-as-start">
-                        <label for="same-as-start">Same as Start Date</label>
-                    </span>
-                    <input type="date" name="end_date" class="form-control" id="end-date" aria-describedby="dateHelp">
+                <div class="form-group col-md-12">
+                    <label for="start-date">Date</label>
+                    <input type="date" name="start_date" class="form-control {{$errors->has('start_date') ? 'is-invalid' : '' }}" id="start-date" aria-describedby="dateHelp">
+                    @if($errors->has('start_date'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('start_date')}}
+                        </span>
+                    @endif
                 </div>
             </div>
             <div class="form-group">
                 <label for="activity">Activity</label>
-                <input type="text" class="form-control" name="activity" id="activity" placeholder="e.g. Run, Walk, Skate, Surf, etc.">
+                <input type="text" class="form-control {{$errors->has('activity') ? 'is-invalid' : '' }}" name="activity" id="activity" placeholder="e.g. Run, Walk, Skate, Surf, etc.">
+                @if($errors->has('activity'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('activity')}}
+                        </span>
+                @endif
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="time">Time (in minutes)</label>
-                    <input type="number" name="time" class="form-control" id="time" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    <input type="number" name="time" class="form-control {{$errors->has('time') ? 'is-invalid' : '' }}" id="time" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    @if($errors->has('time'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('time')}}
+                        </span>
+                    @endif
                 </div>
                 <div class="form-group col-md-6">
                     <label for="distance">Distance (in kilometres)</label>
-                    <input type="number" name="distance" class="form-control" id="distance" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    <input type="number" name="distance" class="form-control {{$errors->has('distance') ? 'is-invalid' : '' }}" id="distance" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    @if($errors->has('distance'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('distance')}}
+                        </span>
+                    @endif
                 </div>
             </div>
             <a href="{{ URL()->previous() }}" class="btn btn-danger float-left">x Cancel</a>
--- my-oranga/resources/views/activities/edit.blade.php
+++ my-oranga/resources/views/activities/edit.blade.php
@@ -10,32 +10,45 @@
     <div class="container">
         <form action="{{route('activities.update', $activity->id)}}" method="post" class="form">
             @csrf
+            {{ method_field('PATCH') }}
             <div class="form-row">
-                <div class="form-group col-md-6">
+                <div class="form-group col-md-12">
                     <label for="start-date">Start Date</label>
-                    <input type="date" value="{{$activity->date}}" name="start-date" class="form-control" id="start-date" aria-describedby="dateHelp">
-                </div>
-                <div class="form-group col-md-6">
-                    <span>
-                        <label for="end-date" class="pr-4">End Date</label>
-                        <input type="checkbox" id="same-as-start">
-                        <label for="same-as-start">Same as Start Date</label>
-                    </span>
-                    <input type="date" name="end-date" class="form-control" id="end-date" aria-describedby="dateHelp">
+                    <input type="date" value="{{$activity->date}}" name="start_date" class="form-control {{$errors->has('start_date') ? 'is-invalid' : '' }}" id="start-date" aria-describedby="dateHelp">
+                    @if($errors->has('start_date'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('start_date')}}
+                        </span>
+                    @endif
                 </div>
             </div>
             <div class="form-group">
                 <label for="activity">Activity</label>
-                <input type="text" value="{{$activity->type}}" class="form-control" id="activity" placeholder="e.g. Run, Walk, Skate, Surf, etc.">
+                <input type="text" value="{{$activity->type}}" class="form-control {{$errors->has('activity') ? 'is-invalid' : '' }}" id="activity" placeholder="e.g. Run, Walk, Skate, Surf, etc.">
+                @if($errors->has('activity'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('activity')}}
+                        </span>
+                @endif            
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="time">Time (in minutes)</label>
-                    <input type="number" value="{{$activity->minutes}}" name="time" class="form-control" id="time" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    <input type="number" value="{{$activity->minutes}}" name="time" class="form-control {{$errors->has('activity') ? 'is-invalid' : '' }}" id="time" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    @if($errors->has('time'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('time')}}
+                        </span>
+                    @endif
                 </div>
                 <div class="form-group col-md-6">
                     <label for="distance">Distance (in kilometres)</label>
-                    <input type="number" value="{{$activity->distance}}" name="distance" class="form-control" id="distance" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    <input type="number" value="{{$activity->distance}}" name="distance" class="form-control {{$errors->has('distance') ? 'is-invalid' : '' }}" id="distance" aria-describedby="numberHelp" step="0.01" placeholder="0">
+                    @if($errors->has('distance'))
+                        <span class="invalid-feedback font-weight-bold">
+                            * {{$errors->first('distance')}}
+                        </span>
+                    @endif
                 </div>
             </div>
             <a href="{{ URL()->previous() }}" class="btn btn-danger float-left">x Cancel</a>
