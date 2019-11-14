4fcd4eb4e636c14601e1b88fa645a235f53e2ade
Isaac Underwood <undeis1@student.op.ac.nz>
2019-11-14 23:57:51 +1300

    change home page

--- my-oranga/resources/views/home.blade.php
+++ my-oranga/resources/views/home.blade.php
@@ -2,23 +2,36 @@
 
 @section('main-class', 'main-home')
 @section('content')
-<div class="container">
-    <div class="mt-4">
-    <h1 class="text-left text-h1-large">A Wellness Tool Designed for You.</h1>
+<div class="position-ref full-height home-landing">
+    <div class="row mx-auto">
+        <div class="col-md-12">
+            <h1 class="h1-large text-center d-inline-block brand-handwriting">My</h1><h1 class="h1-large text-center d-inline-block">Oranga</h1>
+        </div>
     </div>
-    <div class="flex-center position-ref full-height">
-        <div class="col-md-8">
-            <blockquote class="blockquote">
-                <h1 class="mb-2">"If you can dream it, you can do it."</h1>
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="my-2">A Wellness Tool Designed for You.</h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="my-4">Get Started</h2>
+        </div>
+    </div>
+    <div class="row text-center">
+        <div class="col-md-6 text-right">
+            <a href="{{route('login')}}" class="btn btn-outline-primary btn-lg mx-2">Login</a>
+        </div>
+        <div class="col-md-6 text-left">
+            <a href="{{route('register')}}" class="btn btn-outline-primary btn-lg mx-2">Register</a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <blockquote class="blockquote quote-home">
+                <h2 class="mb-2 quote-home">"If you can dream it,<br>you can do it."</h2>
                 <footer class="blockquote-footer">Walt Disney</footer>
             </blockquote>
-        </div>
-        <div class="col-md-4 border-left">
-            <div class="mx-auto text-center">
-                <h3 class="py-3">Get Started.</h3>
-                <a href="{{route('login')}}" class="btn btn-outline-dark btn-lg mx-2">Login</a>
-                <a href="{{route('register')}}" class="btn btn-outline-dark btn-lg mx-2">Register</a>
-            </div>
         </div>
     </div>
 </div>
--- my-oranga/resources/views/layouts/app.blade.php
+++ my-oranga/resources/views/layouts/app.blade.php
@@ -12,18 +12,20 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/loading-bar.css')}}"/>
+    <script type="text/javascript" src="{{asset('js/loading-bar.js')}}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="https://kit.fontawesome.com/b42a9b5167.js" crossorigin="anonymous"></script>
-    
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
@@ -38,7 +40,7 @@
             }
 
             .full-height {
-                height: 100vh;
+                height: 90vh;
             }
 
             .flex-center {
@@ -82,7 +84,7 @@
 </head>
 <body>
     <div id="app">
-        {{--@include('inc.navbar')--}}
+        @include('inc.navbar')
 
         <main class="py-4 @yield('main-class')">
             @yield('content')
