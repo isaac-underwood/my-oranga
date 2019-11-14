a009821d8466a95050ed2366f4638913501e9bfd
Isaac Underwood <undeis1@student.op.ac.nz>
2019-11-14 23:53:02 +1300

    update sleep controller

--- my-oranga/app/Http/Controllers/SleepController.php
+++ my-oranga/app/Http/Controllers/SleepController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Sleep;
+use Auth;
 
 class SleepController extends Controller
 {
@@ -23,7 +25,8 @@ class SleepController extends Controller
      */
     public function create()
     {
-        //
+        $last_sleep = Sleep::where('user_id', Auth::user()->id)->latest('date')->first();
+        return view('sleep.create', compact('last_sleep'));
     }
 
     /**
@@ -34,7 +37,31 @@ class SleepController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //validation rules
+        $rules = [
+            'date' => 'required|date',
+            'hours' => 'required|numeric|min:0.25|max:99.99'
+        ];
+
+        //custom validation error messages
+        $messages = [
+            'date.required' => 'Please enter the date that your sleep started on.',
+            'hours.required' => 'Please enter the amount of hours you slept.',
+            'hours.min' => 'The minimum sleep you can enter is 15 minutes.',
+            'hours.max' => 'It is not possible to sleep for this long. Please check your input and try again.'
+        ];
+
+        $request->validate($rules, $messages);
+        $sleep = new Sleep;
+
+        $sleep->user_id = Auth::user()->id;
+        $sleep->date = $request->date;
+        $sleep->hours = $request->hours;
+        $sleep->save();
+
+        return redirect()
+            ->route('home')
+            ->with('status','You added a new sleep record for ' . $sleep->date . '. You slept for ' . $sleep->hours . ' hours.');
     }
 
     /**
@@ -56,7 +83,8 @@ class SleepController extends Controller
      */
     public function edit($id)
     {
-        //
+        $sleep = Sleep::findOrFail($id);
+        return view('sleep.edit', compact('sleep'));
     }
 
     /**
@@ -68,7 +96,31 @@ class SleepController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //validation rules
+        $rules = [
+            'date' => 'required|date',
+            'hours' => 'required|numeric|min:0.25|max:99.99'
+        ];
+
+        //custom validation error messages
+        $messages = [
+            'date.required' => 'Please enter the date that your sleep started on.',
+            'hours.required' => 'Please enter the amount of hours you slept.',
+            'hours.min' => 'The minimum sleep you can enter is 15 minutes.',
+            'hours.max' => 'It is not possible to sleep for this long. Please check your input and try again.'
+        ];
+
+        $request->validate($rules, $messages);
+        $sleep = Sleep::find($id);
+
+        $sleep->user_id = Auth::user()->id;
+        $sleep->date = $request->date;
+        $sleep->hours = $request->hours;
+        $sleep->save();
+
+        return redirect()
+            ->route('home')
+            ->with('status','You updated a sleep record for ' . $sleep->date . '. You slept for ' . $sleep->hours . ' hours.');
     }
 
     /**
@@ -79,6 +131,10 @@ class SleepController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $sleep = Sleep::find($id);
+        $sleep->delete();
+        return redirect()
+        ->route('home')
+        ->with('status','You successfully deleted a sleep record');
     }
 }
