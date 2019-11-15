ddd6982bda8b4eaa4b27391fbe937d9fa2b1bb0f
Isaac Underwood <undeis1@student.op.ac.nz>
2019-11-15 14:51:14 +1300

    add horizontal rules

--- my-oranga/resources/views/home.blade.php
+++ my-oranga/resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <div class="row mx-auto">
         <div class="col-md-12">
             <h1 class="h1-large text-center d-inline-block brand-handwriting">My</h1><h1 class="h1-large text-center d-inline-block">Oranga</h1>
+            <hr class="hr-white-home">
         </div>
     </div>
     <div class="row">
@@ -13,11 +14,14 @@
             <h3 class="my-2">A Wellness Tool Designed for You.</h3>
         </div>
     </div>
-    <div class="row">
+    <div class="row home-get-started">
         <div class="col-md-12">
             <h2 class="my-4">Get Started</h2>
+            <hr class="hr-white-home">
         </div>
+        
     </div>
+    
     <div class="row text-center">
         <div class="col-md-6 text-right">
             <a href="{{route('login')}}" class="btn btn-outline-primary btn-lg mx-2">Login</a>
